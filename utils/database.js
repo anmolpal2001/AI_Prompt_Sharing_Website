9c77d4ddff716774c48d7db643916cd35a363391
anmolpal2001 <anmolpal2001@gmail.com>
2024-02-22 02:37:04 +0530

    database.js file updated

--- utils/database.js
+++ utils/database.js
@@ -3,19 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectDB = async () => {
-    mongoose.set('strictQuery',true);
+  mongoose.set("strictQuery", true);
 
-    if(isConnected){
-        console.log("MongoDB is already connected");
-        return;
-    }
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
 
-    try{
-        const conn = await mongoose.connect(process.env.DATABASE_URL)
-        isConnected = true;
-        console.log("MongoDB is connected successfully");
-    }
-    catch(error){
-        console.log("Error in connecting to MongoDB", error);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB is connected successfully");
+  } catch (error) {
+    console.log("Error in connecting to MongoDB", error);
+  }
+};
